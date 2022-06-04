1ca6a110d191550ad41637286aec46a55553e8de
Sir Joseph <sirjosephholmes@gmail.com>
2022-06-04 08:57:09 +0700

    baru bikin layoutnya

---
+++ projek/SuitOsomand12Binar/app/src/main/java/com/example/suitosomand12binar/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.suitosomand12binar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.suitosomand12binar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val _binding: ActivityMainBinding by lazy {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun ambilBtn(){
+
+    }
+}
