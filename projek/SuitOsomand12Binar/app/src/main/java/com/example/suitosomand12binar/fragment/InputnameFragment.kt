88dcee09e7de1aa3e9b6d89f0aa4212a31f71f5a
Sir Joseph <sirjosephholmes@gmail.com>
2022-06-14 07:41:12 +0700

    udh bikin database tp pas di panggil di fragment gk bisa karena view pagernya blom jd dan gk ada contextnya

--- projek/SuitOsomand12Binar/app/src/main/java/com/example/suitosomand12binar/fragment/InputnameFragment.kt
+++ projek/SuitOsomand12Binar/app/src/main/java/com/example/suitosomand12binar/fragment/InputnameFragment.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.suitosomand12binar.R
+import com.example.suitosomand12binar.database.PermainanDatabase
 
-class InputnameFragment: Fragment() {
+class InputnameFragment: Fragment(R.layout.fragment_inputp1) {
+    lateinit var namaPemain: String
+    private val permainanDatabase: PermainanDatabase? by lazy {
+        PermainanDatabase.getInstance(.In)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_inputp1, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    fun inputNama(){
+        val etNama = view?.findViewById<EditText>(R.id.et_nama)
+        val btnSubmit = view?.findViewById<Button>(R.id.btn_submit)
+
+        btnSubmit?.setOnClickListener(){
+            namaPemain = etNama?.text.toString()
+        }
+    }
 }
