0a260c34b553587dca78669665123028804a98e3
Sir Joseph <sirjosephholmes@gmail.com>
2022-06-18 11:03:26 +0700

    ini juga salah

--- projek/binar_suit_chp6/app/src/main/java/com/example/binar_suit_chp6/activity/People.kt
+++
@@ -1,8 +0,0 @@
-package com.example.binar_suit_chp6.activity
-
-import android.graphics.drawable.Drawable
-
-data class People(
-    val name :String,
-    val age :Int
-)
--- projek/cap3/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "Cap3"
-
